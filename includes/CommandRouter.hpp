f1260f556eeb1700c23039c64dd93b655eb725ef
Zay Min Maw - <zmin@z3t2c3.42bangkok.com>
2026-05-06 21:30:25 +0700

    feat: added some cmd router field

--- includes/CommandRouter.hpp
+++ includes/CommandRouter.hpp
@@ -6,7 +6,24 @@
 /*   By: zmin <zmin@student.42bangkok.com>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/06 19:51:39 by zmin              #+#    #+#             */
-/*   Updated: 2026/05/06 19:51:40 by zmin             ###   ########.fr       */
+/*   Updated: 2026/05/06 20:17:48 by zmin             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+
+#ifndef COMMANDROUTER_HPP
+#define COMMANDROUTER_HPP
+
+#include "Server.hpp"
+
+class CommandRouter {
+	private:
+		Server &_server;
+
+		CommandRouter(const CommandRouter &src);
+		CommandRouter &operator=(const CommandRouter &src);
+	public:
+		~CommandRouter();
+};
+
+#endif
