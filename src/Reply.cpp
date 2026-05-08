6108217aa7c183435b63f3cb8c328fb48b0211b1
Zay Min Maw - <zmin@z3t2c3.42bangkok.com>
2026-05-08 20:16:15 +0700

    feat: added success and info replies

--- src/Reply.cpp
+++ src/Reply.cpp
@@ -6,12 +6,44 @@
 /*   By: zmin <zmin@student.42bangkok.com>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/07 12:00:00 by zmin              #+#    #+#             */
-/*   Updated: 2026/05/07 12:00:00 by zmin             ###   ########.fr       */
+/*   Updated: 2026/05/08 20:08:42 by zmin             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-// PHASE 0 STUB. Person B replaces this whole file in their Phase 1.
-// All Reply::* functions are declared in Reply.hpp; their definitions live
-// here once B implements them.
-
 #include "Reply.hpp"
+
+static std::string _fmt(const std::string &sv, const std::string &nr, const std::string &n) {
+	return ":" + sv + " " + nr + " " + (n.empty() ? "*" : n) + " ";	
+}
+
+std::string Reply::welcome(const std::string& serverName, const std::string& nick) {
+	return _fmt(serverName, "001", nick) + ":Welcome to the Internet Relat Network " + nick; 
+}
+
+std::string Reply::yourHost(const std::string& serverName, const std::string& nick) {
+	return _fmt(serverName, "002", nick) + ":Your host is " + serverName + ", running version 1.0";
+}
+
+std::string Reply::created(const std::string& serverName, const std::string& nick) {
+	return _fmt(serverName, "002", nick) + ":This server was created " + "May 2026";
+}
+
+std::string Reply::myInfo(const std::string& serverName, const std::string& nick) {
+	return _fmt(serverName, "002", nick) + serverName + " 1.0 " + "<usermodes> <chanmodes>";
+}
+
+std::string Reply::topic(const std::string& serverName, const std::string& nick, const std::string& chan, const std::string& topic) {
+	return _fmt(serverName, "332", nick) + chan + " :" + topic; 
+}
+
+std::string Reply::noTopic(const std::string& serverName, const std::string& nick, const std::string& chan) {
+	return _fmt(serverName, "331", nick) +  chan + " :No topic is set";
+}
+
+std::string Reply::namReply(const std::string& serverName, const std::string& nick, const std::string& chan, const std::string& names) {
+	return _fmt(serverName, "353", nick) + "= " + chan + " :" + names;
+}
+
+std::string Reply::endOfNames(const std::string& serverName, const std::string& nick, const std::string& chan) {
+	return _fmt(serverName, "366", nick) + chan + ":End of /NAMES list";
+}
