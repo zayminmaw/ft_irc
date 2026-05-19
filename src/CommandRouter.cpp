18da575036d83a5486184bb9d0c7d2b5318dddf6
Zay Min Maw - <zmin@z3t2c3.42bangkok.com>
2026-05-19 20:34:31 +0700

    feat: added handle kick functikon

--- src/CommandRouter.cpp
+++ src/CommandRouter.cpp
@@ -6,7 +6,7 @@
 /*   By: zmin <zmin@student.42bangkok.com>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/07 12:00:00 by zmin              #+#    #+#             */
-/*   Updated: 2026/05/15 18:42:33 by zmin             ###   ########.fr       */
+/*   Updated: 2026/05/19 20:34:07 by zmin             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -389,7 +389,42 @@ void CommandRouter::handleMode(Client& c, const IRCMessage& m) {
 	}
 }
 
-void CommandRouter::handleKick(Client& c, const IRCMessage& m) { if (!_ensureRegistered(c)) return; (void)m; }
+void CommandRouter::handleKick(Client& c, const IRCMessage& m) { 
+	if (!_ensureRegistered(c)) return;
+	if (m.params.size() < 2) {
+		c.queueOutbound(Reply::errNeedMoreParams(_server.getName(), c.getNick(), m.command));
+		return;
+	}
+	std::string chanName = m.params[0];
+	std::string targetNick = m.params[1];
+	std::string reason = (m.params.size() > 2) ? m.params[2] : c.getNick();
+	Channel* ch = _server.findChannel(chanName);
+	if (!ch) {
+		c.queueOutbound(Reply::errNoSuchChannel(_server.getName(), c.getNick(), chanName));
+		return;
+	}
+	if (!ch->hasMember(&c)) {
+		c.queueOutbound(Reply::errNotOnChannel(_server.getName(), c.getNick(), ch->getName()));
+		return;
+	}
+	if (!ch->isOperator(&c)) {
+		c.queueOutbound(Reply::errChanOPrivsNeeded(_server.getName(), c.getNick(), ch->getName()));
+		return;
+	}
+	Client* targetClient = _server.findClientByNick(targetNick);
+	if (!targetClient || !ch->hasMember(targetClient)) {
+		c.queueOutbound(Reply::errUserNotInChan(_server.getName(), c.getNick(), targetNick, ch->getName()));
+		return;
+	}
+	ch->broadcast(Reply::kickMsg(c.getPrefix(), ch->getName(), targetNick, reason), NULL);
+	ch->removeMember(targetClient);
+	if (ch->getOperators().empty() && !ch->getMembers().empty()) {
+		Client* newOp = ch->getMembers()[0];
+		ch->addOperator(newOp);
+		ch->broadcast(Reply::modeMsg(_server.getName(), ch->getName(), "+o", newOp->getNick()),NULL);
+	}
+	_server.removeChannelIfEmpty(chanName);
+}
 void CommandRouter::handleInvite(Client& c, const IRCMessage& m) { if (!_ensureRegistered(c)) return; (void)m; }
 
 void CommandRouter::handlePrivmsg(Client& c, const IRCMessage& m) { 
